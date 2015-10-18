34b68adb3817af74a7f6a776a89ae36a60c904a1
Carl Kinghorn <carl_kinghorn@live.com>
2015-10-18 09:33:03 -0500

    Naive match + while with mutable iterator

---
+++ FsharpFizzBuzz/Program.fs
@@ -0,0 +1,24 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+let MatchFizzBuzz x =
+    match x with
+    | x when x % 3 = 0 && x % 5 = 0 -> "FizzBuzz"
+    | x when x % 3 = 0 -> "Fizz"
+    | x when x % 5 = 0 -> "Buzz"
+    | _ -> x.ToString()
+
+let IterativeFizzBuzz start max =
+    let mutable i = start
+    let mutable acc = ""
+    while i <= max do
+        acc <- acc + (MatchFizzBuzz i) + "\n"
+        i <- i+1
+    acc
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Iterative:\n%s" (IterativeFizzBuzz 1 100)
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
