0a306a35c1f0543db0940383c29cff8b4df00e00
Carl Kinghorn <carl_kinghorn@live.com>
2015-10-18 10:50:26 -0500

    Add recursive version to print FizzBuzz

---
+++ FsharpFizzBuzz/NaiveFizzBuzz.fs
@@ -0,0 +1,16 @@
+﻿module NaiveFizzBuzz
+
+let MatchFizzBuzz x =
+    match x with
+    | x when x % 3 = 0 && x % 5 = 0 -> "FizzBuzz"
+    | x when x % 3 = 0 -> "Fizz"
+    | x when x % 5 = 0 -> "Buzz"
+    | _ -> x.ToString()
+
+let IterativeFizzBuzz start max =
+    let mutable i = start
+    let mutable acc = ""
+    while i <= max do
+        acc <- acc + (MatchFizzBuzz i) + "\n"
+        i <- i+1
+    acc
